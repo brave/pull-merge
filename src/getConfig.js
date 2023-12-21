b613e1d6b9eeeed026b998204251a7756b233c83
Andrea Brancaleoni <ab.gh@pompel.me>
2023-12-21 12:42:41 +0100

    *: refactor+get renovate patch diff

---
+++ src/getConfig.js
@@ -0,0 +1,21 @@
+export default async function getConfig({ owner, repo, path, github, githubToken, debug = false }) {
+    if (!github && githubToken) {
+        const { Octokit } = await import("@octokit/core");
+
+        github = new Octokit({ auth: githubToken })
+    }
+
+    try {
+        const { data } = await github.rest.repos.getContent({
+            owner,
+            repo,
+            path,
+        });
+        const fileContent = Buffer.from(data.content, 'base64').toString('utf8');
+        if (debug) console.log(fileContent);
+        return JSON.parse(fileContent);
+    } catch (err) {
+        if (debug) console.log(err);
+        return {};
+    }
+}
