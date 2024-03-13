6e45a31ed8a7bfc1b75f4dcf933a424d98f1df0b
yan <yan@brave.com>
2024-03-13 09:37:46 -0700

    New workflow: nodejs lint
    - add standardjs and pre-merge action
    - output from standard --fix
    - manual lint fixes
    - disable camelcase rule when needed, fix vars
    - avoid confusing const inside loop
    - node.js.yml: fix npm lint pass
    
    Signed-off-by: Andrea Brancaleoni <abc@pompel.me>

--- src/getPatch.js
+++ src/getPatch.js
@@ -1,60 +1,62 @@
-export default async function getPatch({ owner, repo, prnum,
-    githubToken = null,
-    github = null,
-    runIfPrivate = false,
-    debug = false
+export default async function getPatch ({
+  owner, repo, prnum,
+  githubToken = null,
+  github = null,
+  runIfPrivate = false,
+  debug = false
 }) {
-    if (!github && githubToken) {
-        const { Octokit } = await import("@octokit/core");
+  if (!github && githubToken) {
+    const { Octokit } = await import('@octokit/core')
 
-        github = new Octokit({ auth: githubToken })
-    }
-    
-    if (debug)
-        console.log(`getPatch ${owner} ${repo} ${prnum}`);
+    github = new Octokit({ auth: githubToken })
+  }
 
-    var patchBody = null;
+  if (debug) { console.log(`getPatch ${owner} ${repo} ${prnum}`) }
 
-    if (!github && !githubToken) {
-        const patchResponse = await fetch(`https://github.com/${owner}/${repo}/pull/${prnum}.diff`);
+  let patchBody = null
 
-        if (patchResponse.status != 200)
-            throw new Error(`Could not fetch PR diff: ${patchResponse.status} ${patchResponse.statusText}`);
+  if (!github && !githubToken) {
+    const patchResponse = await fetch(`https://github.com/${owner}/${repo}/pull/${prnum}.diff`)
 
-        patchBody = await patchResponse.text();
-    } else {
-        const { data: pBody } = await github.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
-            owner: owner,
-            repo: repo,
-            pull_number: prnum,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            },
-            mediaType: {
-                format: "diff",
-            },
-        })
-
-        const { data: repoResponse } = await github.request('GET /repos/{owner}/{repo}', {
-            owner: owner,
-            repo: repo,
-            pull_number: prnum,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        })
-
-        if (!runIfPrivate && (repoResponse.private || repoResponse.visibility == "private"))
-            throw new Error("This repo is private, and you have not enabled runIfPrivate");
-
-        patchBody = pBody;
+    if (patchResponse.status !== 200) {
+      throw new Error(`Could not fetch PR diff: ${patchResponse.status} ${patchResponse.statusText}`)
     }
 
-    return {
-        repo: repo,
-        owner: owner,
-        type: "simple",
-        body: patchBody,
-        watermark: `[[puLL-Merge](https://github.com/brave/pull-merge)] - [${owner}/${repo}@${prnum}](https://github.com/${owner}/${repo}/pull/${prnum})`
-    };
+    patchBody = await patchResponse.text()
+  } else {
+    const { data: pBody } = await github.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+      owner,
+      repo,
+      pull_number: prnum,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      },
+      mediaType: {
+        format: 'diff'
+      }
+    })
+
+    const { data: repoResponse } = await github.request('GET /repos/{owner}/{repo}', {
+      owner,
+      repo,
+      pull_number: prnum,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+    if (!runIfPrivate && (repoResponse.private || repoResponse.visibility === 'private')) {
+      throw new Error('This repo is private, and you have not enabled runIfPrivate')
+    }
+
+    patchBody = pBody
+  }
+
+  return {
+    repo,
+    owner,
+    type: 'simple',
+    body: patchBody,
+    watermark: `[[puLL-Merge](https://github.com/brave/pull-merge)] - [${owner}/${repo}@${prnum}](https://github.com/${owner}/${repo}/pull/${prnum})`
+  }
 }
--- src/getProperties.js
+++ src/getProperties.js
@@ -1,25 +1,25 @@
-export default async function getProperties({ owner, repo, github, githubToken, debug = false }) {
-    if (!github && githubToken) {
-        const { Octokit } = await import("@octokit/core");
+export default async function getProperties ({ owner, repo, github, githubToken, debug = false }) {
+  if (!github && githubToken) {
+    const { Octokit } = await import('@octokit/core')
 
-        github = new Octokit({ auth: githubToken })
-    }
+    github = new Octokit({ auth: githubToken })
+  }
 
-    try {
-        let properties = await github.request('GET /repos/{owner}/{repo}/properties/values', {
-            owner: owner,
-            repo: repo,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        });
-        if (debug) console.log(properties);
-        return properties.data.reduce((acc, cur) => {
-            acc[cur.property_name] = cur.value;
-            return acc;
-        }, {});
-    } catch (err) {
-        console.log(err);
-        return {};
-    }
+  try {
+    const properties = await github.request('GET /repos/{owner}/{repo}/properties/values', {
+      owner,
+      repo,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+    if (debug) console.log(properties)
+    return properties.data.reduce((acc, cur) => {
+      acc[cur.property_name] = cur.value
+      return acc
+    }, {})
+  } catch (err) {
+    console.log(err)
+    return {}
+  }
 }
