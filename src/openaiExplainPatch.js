be3a85d0080d82db13d8e8f4f09d878b47ead1a3
Andrea Brancaleoni <abc@pompel.me>
2024-03-13 18:36:18 +0100

    *: use latest GPT-4 models

--- src/openaiExplainPatch.js
+++ src/openaiExplainPatch.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { encoding_for_model } from "tiktoken";
 
 export default async function explainPatch({apiKey, patchBody, owner, repo,
-  models = ["gpt-3.5-turbo-0125"],
+  models = ["gpt-4-0125-preview", "gpt-3.5-turbo-0125"],
   system = `
 You are an expert software engineer reviewing a pull request on Github. Lines that start with "+" have been added, lines that start with "-" have been deleted. Use markdown for formatting your review.
 
